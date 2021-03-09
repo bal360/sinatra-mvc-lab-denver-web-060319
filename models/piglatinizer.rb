216033c398ef3bf4284299f22dcc6097e1d7ab6f
Blake A. Long <blake.a.long@gmail.com>
2021-03-08 17:16:08 -0700

    commit tree 505e9a995ebcc1c672d6101c2fb76a4add46f7ab

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -3,6 +3,7 @@ class PigLatinizer
     def piglatinize(string)
         string = string.split(" ").map do |word|
             first_letter = word.match(/^([^aeiouAEIOU]+)/)
+            binding.pry
             if first_letter
                 "#{word.gsub(/^([^aeiouAEIOU])+/, "")}#{first_letter}ay"
             else 
@@ -11,5 +12,4 @@ class PigLatinizer
         end
         string.join(" ")
     end
-    
 end
